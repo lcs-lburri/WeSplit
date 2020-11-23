c3cd9eb167f6f567b2fa157f881a498c48d59851
Luke Burri <LBUrri@lcs.on.ca>
2020-11-23 12:11:58 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Burri, Luke on 2020-11-23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
