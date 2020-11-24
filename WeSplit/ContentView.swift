59e7f5b4984062fdf778deb6c54cd146d3dc45b9
Luke Burri <LBUrri@lcs.on.ca>
2020-11-24 14:46:02 -0500

    added three input values there are yellow errors needed to be fixed

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,9 +12,18 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     
-    let tipPersentages = [10, 15, 20, 25, 0]
+    let tipPersentages = [0, 10, 15, 20, 25,]
     
-    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPersentages[tipPercentage])
+        return 0
+        
+        let stringValue = "0.5"
+        let doubleValue = Double(stringValue)
+        let  orderAmount = Double(checkAmount) ?? 0
+        
+    }
     var body: some View {
         NavigationView {
             Form {
@@ -29,13 +38,14 @@ struct ContentView: View {
                     }
                 }
                 
-                Section {
+                Section(header: Text("how much tip do you want to leave?")) {
+                    
                     Picker ("tip percentage", selection: $tipPercentage){
                         ForEach(0..<tipPersentages.count) {
                             Text ("\(self.tipPersentages[$0])%")
                         }
                     }
-                    pickerStyle(SegmentedPickerStyle())
+                    .pickerStyle(SegmentedPickerStyle())
                 }
                 Section{
                     Text ("$\(checkAmount)")
@@ -44,9 +54,10 @@ struct ContentView: View {
             .navigationBarTitle("WeSpit")
         }
     }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
